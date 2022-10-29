dc177e051dece84dcec398769d85a0622ca4df40
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-30 00:39:04 +0600

    brandScheema updated

--- models/Brand.js
+++ models/Brand.js
@@ -2,22 +2,56 @@ const mongoose = require("mongoose");
 
 const validator = require("validator");
 
-const brandSchema = mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, "Please provide a brand name"],
-    maxLength: 100,
-    unique: true,
+const { ObjectId } = mongoose.Schema.Types;
+
+const brandSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a brand name"],
+      maxLength: 100,
+      unique: true,
+      lowercase: true,
+    },
+    description: String,
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Please provide a valid email"],
+      lowercase: true,
+    },
+    website: {
+      type: String,
+      validate: [validator.isUrl, "Please provide a valid website url"],
+    },
+    location: String,
+    products: [
+      {
+        type: ObjectId,
+        ref: "Product",
+      },
+    ],
+    suppliers: [
+      {
+        name: String,
+        contactNumber: String,
+        id: {
+          type: ObjectId,
+          ref: "Supplier",
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
   },
-  description: String,
-  email: {
-    type: String,
-    validate: [validator.isEmail, "Please provide a valid email"],
-  },
-  website: {
-    type: String,
-    validate: [validator.isUrl, "Please provide a valid website url"],
-  },
-  location: String
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+module.exports = Brand;
