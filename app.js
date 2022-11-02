6ff3f45319036e573700b142f3ef276d78c28fc7
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-03 02:46:20 +0600

    brand and product route

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
+const productRoute = require("./routes");
+const brandRoute = require("./routes/brandRoute");
+
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
+
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/brand", brandRoute);
 
 module.exports = app;
